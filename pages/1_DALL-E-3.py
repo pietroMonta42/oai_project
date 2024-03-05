bac929a874594289f4cf66859e334a023da3a171
TreRote42 <pietrimonta8@gmail.com>
2024-03-05 20:13:07 +0100

    fix for dalle

--- pages/1_DALL-E-3.py
+++ pages/1_DALL-E-3.py
@@ -37,28 +37,37 @@ st.caption("Generative Image Model")
 if "generating" not in st.session_state:
     st.session_state["generating"] = False
 
-if (prompt := st.text_input("Prompt for image generation", disabled=st.session_state['generating'])) and not st.session_state['generating']:
-    logger.info(f"prompt: {prompt}")
-    st.session_state["generating"] = True
+if ( not st.session_state['generating'] ):
+    if prompt := st.text_input("Prompt for image generation"):
+        st.session_state["generating"] = True
+        logger.info(f"prompt: {prompt}")
+        st.session_state["prompt"] = prompt
+        st.rerun()
+    
+if st.session_state["generating"]:
     with st.spinner("Generating image..."):
-        image=op.create_image(prompt)
-        st.image(image[0], use_column_width=True)
-        logger.info(f"image: {image[0]}")
-        st.write("Image generated at:" + image[0])
-        st.session_state["last_image"] = image[0]
-        image_name = image[0][117:149]
-        logger.info(f"image_name: {image_name}")
-        if False:
-            save_path = os.path.join("images", image_name)
-            with open(save_path, 'wb') as image_file:
-                image_file.write(requests.get(image[0]).content)
-                logger.info(f"image_file saved at: {save_path}")
-        else:
-            if st.download_button("Save Image", requests.get(image[0]).content, f"{image_name}", "Save image to your computer"):
-                logger.info(f"image_file saved at: {image_name}")
-    st.session_state["generating"] = False
+        if "prompt" in st.session_state and st.session_state["prompt"] is not None:
+            st.session_state["image"] = op.create_image(st.session_state["prompt"])
+            st.session_state["prompt"] = None
             
-        
+if "image" in st.session_state and st.session_state["image"] is not None:
+    image = st.session_state["image"]
+    st.image(image[0], use_column_width=True)
+    logger.info(f"image: {image[0]}")
+    st.write("Image generated at:" + image[0])
+    image_name = image[0][117:149]
+    logger.info(f"image_name: {image_name}")
+    if False:
+        save_path = os.path.join("images", image_name)
+        with open(save_path, 'wb') as image_file:
+            image_file.write(requests.get(image[0]).content)
+            logger.info(f"image_file saved at: {save_path}")
+    else:
+        if st.download_button("Save Image", requests.get(image[0]).content, f"{image_name}", "Save image to your computer"):
+            logger.info(f"image_file saved at: {image_name}")
+                   
     if st.button("Generate another image"):
+        st.session_state["last_image"] = image[0]
         st.session_state["generating"] = False
+        st.session_state["image"] = None
         st.rerun()
